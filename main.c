aca2e2ee2cd0219bfde6817e24cc59dbb5f9daf6
char-1ee <xingjianli59@gmail.com>
2021-12-17 23:52:04 +0800

    Update main.c

--- main.c
+++ main.c
@@ -10,8 +10,8 @@ int sh_cd(char **args);
 int sh_help(char **args);
 int sh_exit(char **args);
 
-/*
-    List of builtin commands, followed by their corresponding functions.
+/**
+ * @brief List of builtin commands, followed by their corresponding functions. 
  */
 char * builtin_str[] = {
     "cd", 
@@ -29,23 +29,30 @@ int sh_num_builtins() {
     return sizeof(builtin_str) / sizeof(char *);
 }
 
-//------------------------------------------------------------------------------------------
-
-/*
-    Builtin function implementations.
-*/
+/**
+ * @brief Built-in shell commands: cd.
+ */
 int sh_cd(char ** args) {
-    if (args[1] == NULL) {      // check if 2nd argument exists
+
+    // check if 2nd argument exists
+    if (args[1] == NULL) {
 
         /* TODO: can return to root dir */
 
-        fprintf(stderr, "sh: expected argument to \" cd \" \n"); // If cd follows no PATH, report error
+        // If cd follows no PATH, report error
+        fprintf(stderr, "sh: expected argument to \" cd \" \n"); 
+
     } else {
-        if (chdir(args[1]) != 0) {  // chdir() changes directory and checks error
+
+        // chdir() changes directory and checks error
+        if (chdir(args[1]) != 0) {
             perror("sh");
         }
     }
 
+/**
+ * @brief Built-in shell command: help.
+ */
 int sh_help(char ** args) {
     int i;
     printf("Please refer to built-in commands below: \n");
@@ -55,14 +62,118 @@ int sh_help(char ** args) {
     return 1;
 }
 
+/**
+ * @brief Built-in shell command: exit.
+ */
 int sh_exit(char ** args) {
     return 0;
 }
 
-//------------------------------------------------------------------------------------------
+/**
+ * @brief Reading a line.
+ */
+char * sh_read_line(void) {
+    int buffsize = SH_RL_BUFSIZE;
+    int position = 0;
+    int c;
+    char * buffer = malloc(sizeof(char) * buffsize);
+
+    if (!buffer) {
+        fprintf(stderr, "sh: intial allocation error \n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        // read a charactor
+        c = getchar();
+
+        // when hit EOF or escape, replace it with null charactor and return
+        if (c == EOF || c = '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+    }   
+
+    // a common strategy: when exceed buffersize, we reallocate the buffer
+    if (position >= buffsize) {
+        buffsize += SH_RL_BUFSIZE;
+        buffer = realloc(buffer, buffersize);
+        if (!buffer) {
+            fprintf(stderr. "sh: reallocation error \n");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+/**
+ * @brief Parsing the line. 
+ * 
+ */
+
+// TODO
+
+
+/**
+ * @brief Putting together builtins and processes.
+ * 
+ * @param args 
+ * @return int 
+ */
+int sh_execute(char ** args) {
+    int i;
+
+    // an empty command was entered
+    if (args[0] == NULL) {
+        return 1;
+    }
+
+    for (i = 0; i < sh_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[0]) == 0)
+            return (*builtin_func[i])(args);
+    }
+
+    return sh_launch(args);
+}
+
+// TODO
+int sh_launch(char ** args) {
+    pid_t pis, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        id ()
+    }
+}
+
+/**
+ * @brief A loop to input.
+ * 
+ */
+void sh_loop(void) {
+    char * line;
+    char ** args;
+    int status;
+
+    do {
+        printf("> ");
+        line = sh_read_line();
+        args = sh_split_line(line);
+        status = sh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status)
+}
 
 int main(int argc, char ** argv) {      // TODO: add command hint/correction machanism? Refer to RRPSS  
+    
+    // Run command loop
     sh_loop();
+
     return EXIT_SUCCESS;
 }
 
