a5c91ef6406b0a76e0944df692e3fb393b7c673f
char-1ee <xingjianli59@gmail.com>
2022-03-24 21:09:25 +0800

    Add singal handling

--- src/main.c
+++ src/main.c
@@ -4,12 +4,18 @@
 #include <readline/readline.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
+#include <setjmp.h>
 
 #define TRUE 1
 #define BUFF_SIZE 100
 
 char **read_command(char *);
-int cd(chat *);
+int cd(char *);
+void sigint_handler(int);
+
+static sigjmp_buf env;
+static volatile sig_atomic_t jump_flag = 0;
 
 int main() {
     char **command;
@@ -17,8 +23,26 @@ int main() {
     pid_t child_pid;
     int sta_loc;
 
+    /* Setup SIGINT */
+    struct sigaction s;
+    s.sa_handler = sigint_handler;
+    sigemptyset(&s.sa_mask);
+    s.sa_flags = SA_RESTART;
+    sigaction(SIGINT, &s, NULL);
+
     while (TRUE) {
+        if (sigsetjmp(env, 1) == 42) {
+            printf("\n");
+            continue;
+        }
+
+        jump_flag = 1;
         input = readline("csh> ");
+        if (input == NULL) {
+            printf("\n");
+            exit(0);
+        }
+
         command = read_command(input);
 
         /* handle empty command */
@@ -46,6 +70,11 @@ int main() {
         }
 
         if (child_pid == 0) {
+            struct sigaction s_child;
+            s_child.sa_handler = sigint_handler;
+            sigemptyset(&s_child.sa_mask);
+            s_child.sa_flags = SA_RESTART;
+            sigaction(SIGINT, &s_child, NULL);
 
             /* if return -1, exxcution failed */
             if (execvp(command[0], command) < 0) {
@@ -56,15 +85,17 @@ int main() {
             waitpid(child_pid, &sta_loc, WUNTRACED);
         }
 
-        free(input);
-        free(command);
+        if (!input)
+            free(input);
+        if (!command)
+            free(command);
     }
     return 0;
 }
 
 /* parse the input line */
 char **read_command(char *input) {
-    char ** command = malloc(BUFF_SIZE * sizeof(char *)));
+    char ** command = malloc(BUFF_SIZE * sizeof(char *));
 
     /* if OS run out of memory */
     if (command == NULL) {
@@ -91,4 +122,10 @@ char **read_command(char *input) {
 /* builtin cd needs we implement it on our own */
 int cd(char *path) {
     return chdir(path);
+}
+
+void sigint_handler(int signo) {
+    if (!jump_flag)
+        return;
+    siglongjmp(env, 42);
 }
